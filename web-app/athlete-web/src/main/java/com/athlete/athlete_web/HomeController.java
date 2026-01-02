931d78a43ab88504fcceb53ec0485d17350f9d86
josepalca27 <josepalma272004@gmail.com>
2026-01-02 14:56:34 -0700

    Render athletes table with Thymeleaf

--- web-app/athlete-web/src/main/java/com/athlete/athlete_web/HomeController.java
+++ web-app/athlete-web/src/main/java/com/athlete/athlete_web/HomeController.java
@@ -5,6 +5,7 @@ import com.athlete.athlete_web.service.AthleteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 import java.util.stream.Collectors;
 
@@ -21,14 +22,9 @@ public class HomeController {
         return "Athlete Web App is running!";
     }
 
-    @GetMapping("/athletes")
-    @ResponseBody
-     public String athletes() {
-        return athleteService.getAthletes()
-                .stream()
-                .map(a -> a.getName()
-                        + " | Age: " + a.getAge()
-                        + " | Sport: " + a.getSport())
-                .collect(Collectors.joining("\n"));
-    }
+@GetMapping("/athletes")
+public String athletes(Model model) {
+    model.addAttribute("athletes", athleteService.getAthletes());
+    return "athletes";
+}
 }
