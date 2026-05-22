39c3fce701ebcd1555c549aaa1ac4179b33eaed9
josepalca27 <josepalma272004@gmail.com>
2026-05-22 11:29:44 -0700

    Add REST API endpoint GET /api/athletes/{name} using path variable

--- web-app/athlete-web/src/main/java/com/athlete/athlete_web/AthleteApiController.java
+++ web-app/athlete-web/src/main/java/com/athlete/athlete_web/AthleteApiController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -30,6 +31,11 @@ public class AthleteApiController {
     public Athlete findAthletes(@RequestParam String name) {
         return athleteService.findAthleteByName(name);
 
+    }
+
+    @GetMapping("athletes/{name}")
+    public Athlete getAthleteByName(@PathVariable String name) {
+        return athleteService.findAthleteByName(name);
     }
 
 }
